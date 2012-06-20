9eac71d79a61b90704e584140135be2a788d99ed
Gergely Imreh <imrehg@gmail.com>
2012-06-20 20:16:55 +0800

    fix humidity detection code, should be more reliable now

--- humidity1/humidity1.ino
+++ humidity1/humidity1.ino
@@ -1,7 +1,10 @@
 static const int VCCPin = 8;
-static const int analogPin = 1;
-static const int R = 15000;
-static const float Vref = 5.0 * 15 / (15 + 51);
+static const int analogPin = 0;
+static const float Re = 50.3; // kOhm
+//static const float Vref = 5.0 * 15 / (15 + 51);
+static const float Vref = 5.0;
+static const float Vm = 2.5;
+
 
 static float a = 2.76366367;
 static float b = -32.40924966;
@@ -12,6 +15,7 @@ int count = 0;
 unsigned long t;
 boolean level = LOW;
 unsigned long nowt;
+static int microwait = 500;
 
 void setup() {
   Serial.begin(19200);
@@ -26,11 +30,11 @@ void loop() {
   if (nowt < t) { 
       t = 0;
   }
-  if ((nowt - t) > 500) {
+  if ((nowt - t) > microwait) {
      digitalWrite(VCCPin, level);
      if (level == HIGH) {
           if (count == 200) {
-             Serial.println(GetHumidity(),1);
+             Serial.println(GetHumidity(),3);
              count = 0;
           } else {
              count++; 
@@ -42,10 +46,22 @@ void loop() {
 }
 
 float GetHumidity(void) {
-  
+  float hum;
   val = analogRead(analogPin);
   float volt = val * 5.0 / 1024;
-  float res = log10(R / (Vref / volt - 1) / 1000);
-  float hum = a * res*res + b * res + c;
+  if (volt < Vm) {
+    hum = 100.0;
+  } else if (volt > Vref) {
+    hum = 0.0;
+  } else {
+    float res = Re / ((Vref-Vm)/(volt-Vm) - 1);
+    float reslog = log10(res);
+    hum = a * reslog*reslog + b * reslog + c;
+    if (hum > 100.0) {
+       hum = 100.0;
+    } else if (hum < 0.0) {
+       hum = 0.0;
+    }
+  }
   return (hum);
 }
