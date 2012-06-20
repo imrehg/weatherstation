a3c75c47be19b85b3fd26d36562fa0a51aa7b062
Gergely Imreh <imrehg@gmail.com>
2012-06-20 16:06:03 +0800

    add Ardunino code for humidity monitor

---
+++ humidity1/humidity1.ino
@@ -0,0 +1,50 @@
+static const int VCCPin = 8;
+static const int analogPin = 1;
+static const int R = 15000;
+
+static float a = 2.76366367;
+static float b = -32.40924966;
+static float c = 102.93566686;
+
+int val = 0;
+int count = 0;
+unsigned long t;
+boolean level = LOW;
+unsigned long nowt;
+
+void setup() {
+  Serial.begin(19200);
+  Serial.println(F("Humidity sensor"));
+  pinMode(VCCPin, OUTPUT);
+  t = micros();
+}
+
+void loop() {
+
+  nowt = micros();
+  if (nowt < t) { 
+      t = 0;
+  }
+  if ((nowt - t) > 500) {
+     digitalWrite(VCCPin, level);
+     if (level == HIGH) {
+          if (count == 200) {
+             Serial.println(GetHumidity(),1);
+             count = 0;
+          } else {
+             count++; 
+          }
+     }
+     level = !level;
+     t = nowt;
+  }  
+}
+
+float GetHumidity(void) {
+  
+  val = analogRead(analogPin);
+  float volt = val * 5.0 / 1024;
+  float res = log10(R / (5 / volt - 1) / 1000);
+  float hum = a * res*res + b * res + c;
+  return (hum);
+}
