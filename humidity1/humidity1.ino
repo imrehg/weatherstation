990e4d8306952b06adf3b17e775995ab9b692b7c
Gergely Imreh <imrehg@gmail.com>
2012-09-11 16:57:21 +0800

    temperature monitoring with arduino

--- humidity1/humidity1.ino
+++ humidity1/humidity1.ino
@@ -1,7 +1,8 @@
 #include <limits.h>  // For ULONG_MAX
 
-static const int VCCPin = 8;
+static const int VCCPin = 9;
 static const int analogPin = 0;
+static const int tempPin = 4;
 static const float Re = 50.3; // kOhm
 //static const float Vref = 5.0 * 15 / (15 + 51);
 static const float Vref = 5.0;
@@ -28,46 +29,96 @@ void setup() {
 }
 
 void loop() {
-
+  float temp, hum, res;
+  
   nowt = micros();
   if (nowt >= t) {
-     deltat = nowt - t;
-  } else {
-     // Taking care of micros overflow that should happen every 70 minutes or so
-     deltat = ULONG_MAX - t + nowt;
+    deltat = nowt - t;
+  } 
+  else {
+    // Taking care of micros overflow that should happen every 70 minutes or so
+    deltat = ULONG_MAX - t + nowt;
   }
   if (deltat > microwait) {
-     digitalWrite(VCCPin, level);
-     if (level == HIGH) {
-          if (count == 200) {
-             Serial.println(GetHumidity(),3);
-             count = 0;
-          } else {
-             count++; 
-          }
-     }
-     level = !level;
-     t = nowt;
+    digitalWrite(VCCPin, level);
+    if (level == HIGH) {
+      if (count == 400) {
+        res = GetResistance();
+
+        digitalWrite(VCCPin, LOW);
+        delay(50);
+        temp = GetTemperature();
+        digitalWrite(VCCPin, HIGH);
+        
+        hum = GetHumidity(res, temp);
+        Serial.print(hum);
+        Serial.print(",");
+        Serial.print(res);
+        Serial.print(",");
+        Serial.print(temp);
+        Serial.println();
+        count = 0;
+      } else {
+        count++; 
+      }
+    }
+    level = !level;
+    t = nowt;
   }  
 }
 
-float GetHumidity(void) {
-  float hum;
+// Output resistance in kOhm
+float GetResistance(void) {
+  float resistance, volt;
+  int val;
+  
   val = analogRead(analogPin);
-  float volt = val * 5.0 / 1024;
+  volt = val * 5.0 / 1024;
+
   if (volt < Vm) {
-    hum = 100.0;
-  } else if (volt > Vref) {
-    hum = 0.0;
-  } else {
-    float res = Re / ((Vref-Vm)/(volt-Vm) - 1);
-    float reslog = log10(res);
-    hum = a * reslog*reslog + b * reslog + c;
-    if (hum > 100.0) {
-       hum = 100.0;
-    } else if (hum < 0.0) {
-       hum = 0.0;
+    resistance = -1;
+  } 
+  else if (volt > Vref) {
+    resistance = -2;
+  } 
+  else {
+    // if in range, calculate resistance value
+    resistance = Re / ((Vref-Vm)/(volt-Vm) - 1);
+  }
+  return (resistance); 
+}
+
+// Rewrite these into two functions so can retrive the resistance
+// separately and use temperature to get correct value;
+float GetHumidity(float resistance, float temperature) {
+  float humidity;
+  
+  if (resistance < 0) {
+     // this means there was some error in the resistance measurement
+     if (resistance == -1) {
+       humidity = 0.0;
+     } else if (resistance == -2) {
+       humidity = 100.0; 
+     } else {
+        humidity = 0.0; 
+     }
+  }
+  else {
+    float reslog = log10(resistance);
+    humidity = a * reslog*reslog + b * reslog + c;
+    if (humidity > 100.0) {
+      humidity = 100.0;
+    } 
+    else if (humidity < 0.0) {
+      humidity = 0.0;
     }
   }
-  return (hum);
+  return (humidity);
 }
+
+float GetTemperature(void) {
+  float temperature;
+  temperature = analogRead(tempPin) * 5.0 / 1024.0 / 0.01;
+  return (temperature);
+}
+
