101973fe3343ac700bfd88fecfb7654443a44cb8
Gergely Imreh <imrehg@gmail.com>
2012-10-23 10:44:28 +0800

    server: update server to latest node modules and code

--- server/server.js
+++ server/server.js
@@ -3,14 +3,38 @@ var express = require('express')
   , ejs = require('ejs')
   , io = require('socket.io')
   , mongoose = require('mongoose')
+  , nconf = require('nconf')
+  , http = require('http')
 ;
 
 var SerialPort = require('serialport2').SerialPort;
 var port = new SerialPort();
 
 // Database related setup
-var db = 'mongoose://'+process.env.MONGO_URL+'/'+process.env.MONGO_DB; 
-mongoose.connect(db);
+nconf.file({ file: 'config.json' });
+var mongos = nconf.get('mongos'),
+    replicaset = nconf.get('replicaset'),
+    database = nconf.get('database');
+var mongooptions = {
+        'db': {
+            'native_parser': true
+        },
+        'server': {
+            'auto_reconnect': true,
+            'poolSize': 10
+        },
+        'replset': {
+            'readPreference': 'nearest',
+            'strategy': 'ping',
+            'rs_name': replicaset,
+            'slaveok': true
+        }
+    };
+var db = mongoose.createConnection(mongos, database, mongooptions);
+db.on('error', console.error.bind(console, 'Database connection error:'));
+db.once('open', function () {
+  console.log("Database connection successful");
+});
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
 var Reading = new Schema({
@@ -18,7 +42,7 @@ var Reading = new Schema({
   , humidity : Number
   , temperature: Number
 });
-var ReadingModel =  mongoose.model('Reading', Reading);
+var ReadingModel = db.model('Reading', Reading);
 // database setup end
 
 var totaldata = '';
@@ -37,16 +61,16 @@ port.open('/dev/ttyUSB0', {
   port.close();
 });
 
-var app = express.createServer(
-    express.logger()
-  , express.static(__dirname + '/public')
-  , express.bodyParser()
-  , express.cookieParser()
-  , express.session({secret: process.env.SESSION_SECRET || 'kdgfcbdgfsgftsrcgsgr'})
-  , express.errorHandler()
-);
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.errorHandler());
+app.use(express.session({secret: process.env.SESSION_SECRET || 'kdgfcbdgfsgftsrcgsgr'}));
+app.use(express.static(__dirname + '/public'));
+var server = http.createServer(app)
 
-var sio = io.listen(app);
+var sio = io.listen(server);
 sio.sockets.on('connection', function (socket) {
     console.log('A socket connected!');
 });
@@ -57,7 +81,13 @@ function handleReading(data) {
     var reading = new ReadingModel(data);
     sio.sockets.emit('humidity', { value: humidity });
     sio.sockets.emit('temperature', { value: temperature });
-    reading.save()
+    reading.save(function (err) {
+      if (err) {
+        console.log("Can't save: "+err);
+      } else {
+        // console.log("Saved!");
+      }
+    });
 }
 
 port.on('data', function(data) {
@@ -88,8 +118,8 @@ app.get("/", function (req, res) {
 	       });
 });
 
-var port = process.env.PORT || 3000;
-
-app.listen(port, function() {
+// Start the app
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
   console.log("Listening on " + port);
 });
